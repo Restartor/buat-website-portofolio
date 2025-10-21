59a72a408e77ca394b1c224453adb9899a0d7a35
Rafi Andiansyah <rafiandiansyah01@gmail.com>
2025-10-21 08:46:51 +0700

    studying javascript & add bgsound in my html lol

---
+++ script.js
@@ -0,0 +1,9 @@
+const audio = document.getElementById("background-audio");
+audio.volume = 0.2;
+
+console.log("Script loaded successfully.");
+let anjay = {
+    nama: "ebew",
+    umur: 20
+}
+console.log(anjay);
